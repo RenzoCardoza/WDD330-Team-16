67e53e3a05c81d8ef03d64afefe085e62dabc43f
Pangloss1107 <pablonsantos1107@gmail.com>
2024-03-02 01:14:14 -0300

    Fixing product.js file to add new items in cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Obtain existin elements of the cart from Lstorage
+  const cartItems = getLocalStorage("so-cart") || [];
+
+  // Add the new product to the list of elements in the cart
+  cartItems.push(product);
+
+  // Save that list in Local Storag
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
