f5f26cba02524e9860491af314685c0fbfc1a6f3
Everton Alves <64697974+Everton543@users.noreply.github.com>
2024-03-02 15:06:27 -0300

    W01 Individual Activity: Add Item to Cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,16 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+ let cartItems = [];
+
+  try{
+    cartItems = getLocalStorage("so-cart");
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  }catch(error){
+    console.error(error);
+    document.querySelector(".product-list").innerHTML = '';
+  }
 }
 
 function cartItemTemplate(item) {
