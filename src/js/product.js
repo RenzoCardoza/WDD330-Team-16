eddf47583f6ea1b39de8b3d5483baf16b13a03bc
Pangloss1107 <pablonsantos1107@gmail.com>
2024-03-09 23:09:23 -0300

    Add a superscript number of items in the cart to the backpack icon.

--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,11 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  // Obtain existin elements of the cart from Lstorage
-  const cartItems = getLocalStorage("so-cart") || [];
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-  // Add the new product to the list of elements in the cart
-  cartItems.push(product);
 
-  // Save that list in Local Storag
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,7 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
 // helper to get parameter strings
 export function getParam(param) {
   const queryString = window.location.search;
@@ -37,3 +38,34 @@ export function renderListWithTemplate(
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
+
+ // Functiin to upload  enumber of items in cart
+ export function updateCartItemCount() {
+  // Obtain a lis of elements of the cart from Local Storage 
+  const cartItems = JSON.parse(localStorage.getItem("so-cart"));
+
+  // Obtain an element in cart
+  const cartElement = document.querySelector(".cart");
+
+  // If there arelements in cart
+  if (cartItems && cartItems.length > 0) {
+    // Create an element to the superindex
+    const badgeElement = document.createElement("sup");
+    badgeElement.classList.add("badge");
+    // Text of the element = quantity of elements in cart
+    badgeElement.textContent = cartItems.length;
+    cartElement.appendChild(badgeElement);
+  }
+}
+
+// Call the function to update cart
+updateCartItemCount();
