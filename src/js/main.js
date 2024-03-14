6bd07da0953bb1fddb1269047978f58d92f765d6
MacPerkins <macdperkins@gmail.com>
2024-03-14 11:24:47 -0600

    formatted and linted code

--- src/js/main.js
+++ src/js/main.js
@@ -12,35 +12,35 @@ listing.init();
 
 // Check if the user has already visited the site by checking for a cookie
 function checkFirstVisit() {
-    if (!getCookie("visited")) {
-        // Show the banner if it's the first visit
-        document.querySelector(".first-banner").style.display = "block";
-        // Set a cookie to remember that the user has visited
-        setCookie("visited", "true", 365);
-    }
+  if (!getCookie("visited")) {
+    // Show the banner if it's the first visit
+    document.querySelector(".first-banner").style.display = "block";
+    // Set a cookie to remember that the user has visited
+    setCookie("visited", "true", 365);
+  }
 }
 
 // Function to set a cookie
 function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
 }
 
 // Function to get the value of a cookie
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
 
 // Call the checkFirstVisit function when the page loads
