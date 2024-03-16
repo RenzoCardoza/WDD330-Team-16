dc10fac75cb8a1f17f4da774e96e56c819ae4e07
Pangloss1107 <pablonsantos1107@gmail.com>
2024-03-16 08:51:03 -0300

    Correct product image size

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,7 +9,7 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor(category) {
+  constructor() {
     // this.category = category;
     // this.path = `../json/${this.category}.json`;
   }
