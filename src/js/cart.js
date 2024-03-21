618333809185ca62a5ad29178b47855166cdc0d7
MacPerkins <macdperkins@gmail.com>
2024-03-20 21:20:29 -0600

    Added team activity with instructors solution filled in as needed

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,53 +1,11 @@
 import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
-
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart") || [];
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  getCartTotalCost(cartItems);
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-}
-
-// function renderCartContents() {
-//   const cartItems = getLocalStorage("so-cart");
-//   const cartItemsArray = Object.values(cartItems);
-
-//   const htmlItems = cartItemsArray.map(item => cartItemTemplate(item)).join("");
-//   document.querySelector(".product-list").innerHTML = htmlItems;
-// }
-
-function getCartTotalCost(cartItems) {
-  if (cartItems.length > 0) {
-    let total = 0;
-    cartItems.forEach((item) => {
-      total += item.FinalPrice;
-    });
-
-    document.querySelector(".cart-total").innerHTML = `Total: $ ${total}`;
-    document.querySelector(".cart-footer").classList.remove("hide");
-    return;
-  }
-  document.querySelector(".cart-footer").classList.add("hide");
-}
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
-}
-
-renderCartContents();
+import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
+
+const cart = new ShoppingCart("so-cart", ".product-list");
+cart.init();
+if (cart.total > 0) {
+  // show our checkout button and total if there are items in the cart.
+  document.querySelector(".list-footer").classList.remove("hide");
+}
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,16 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
 
 loadHeaderFooter();
+
+const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
+myCheckout.init();
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Add event listener after DOM is fully loaded
+    document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+      e.preventDefault();
+      myCheckout.checkout();
+    });
+    document.querySelector("#zip").addEventListener("blur", myCheckout.calculateOrdertotal.bind(myCheckout));
+});
